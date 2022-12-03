87105c2fbbd063e9f548c232541f586bbae6c99c
Kentaro Matsushita <kentaro.m811@gmail.com>
2022-12-03 15:41:13 +0000

    fix: replace Post component using with mdx

--- pages/posts/[slug].tsx
+++ pages/posts/[slug].tsx
@@ -1,16 +1,50 @@
 import { useRouter } from 'next/router'
 import ErrorPage from 'next/error'
-import Container from '../../components/container'
-import PostBody from '../../components/post-body'
-import Header from '../../components/header'
-import PostHeader from '../../components/post-header'
-import Layout from '../../components/layout'
 import { getPostBySlug, getAllPosts } from '../../lib/api'
-import PostTitle from '../../components/post-title'
-import Head from 'next/head'
-import { CMS_NAME } from '../../lib/constants'
-import markdownToHtml from '../../lib/markdownToHtml'
 import type PostType from '../../interfaces/post'
+import { Layout } from '../../components/new/layout'
+import {
+  Heading,
+  Text,
+  Link,
+  UnorderedList,
+  OrderedList,
+  ListItem,
+  Box,
+  Image,
+  Flex,
+  Code,
+  Table,
+  Thead,
+  Tbody,
+  Tr,
+  Th,
+  Td,
+} from '@chakra-ui/react'
+import { serialize } from 'next-mdx-remote/serialize'
+import { MDXRemote } from 'next-mdx-remote'
+
+const components = {
+  a: (props: any) => <Link {...props}>{props.children}</Link>,
+  p: ({ children }: any) => <Text mb={8} lineHeight='tall' >{children}</Text>,
+  h2: ({ children }: any) => <Heading as='h2' fontSize='2xl' lineHeight='base' mt={16} mb={8} >{children}</Heading>,
+  h3: ({ children }: any) => <Heading as='h3' fontSize='xl' lineHeight='base' mt={16} mb={8} >{children}</Heading>,
+  h4: ({ children }: any) => <Heading as='h4' fontSize='lg' fontWeight='bold' lineHeight='base' mt={16} mb={8} >{children}</Heading>,
+  ul: ({ children }: any) => <UnorderedList>{children}</UnorderedList>,
+  ol: ({ children }: any) => <OrderedList>{children}</OrderedList>,
+  li: ({ children }: any) => <ListItem mt={1} mb={1}>{children}</ListItem>,
+  pre: (props: any) => <Box {...props} mb={8} />,
+  // code: (props: any) => <CodeBlock {...props} />,
+  inlineCode: ({ children }: any) => <Code background='rgb(40, 42, 54)' paddingY={0.5} paddingX={1}>{children}</Code>,
+  img: (props: any) => <Image {...props} mb={8} />,
+  table: (props: any) => <Table {...props} mb={8} size="sm" />,
+  thead: (props: any) => <Thead {...props} />,
+  tbody: (props: any) => <Tbody {...props} />,
+  tr: (props: any) => <Tr {...props} />,
+  th: (props: any) => <Th {...props} />,
+  td: (props: any) => <Td {...props} />,
+  // Slide: ({ id }: { id: string }) => <Box mb={8}><Slide id={id} /></Box>
+}
 
 type Props = {
   post: PostType
@@ -24,31 +58,43 @@ export default function Post({ post, morePosts, preview }: Props) {
     return <ErrorPage statusCode={404} />
   }
   return (
-    <Layout preview={preview}>
-      <Container>
-        <Header />
-        {router.isFallback ? (
-          <PostTitle>Loading…</PostTitle>
-        ) : (
-          <>
-            <article className="mb-32">
-              <Head>
-                <title>
-                  {post.title} | Next.js Blog Example with {CMS_NAME}
-                </title>
-                <meta property="og:image" content={post.ogImage.url} />
-              </Head>
-              <PostHeader
-                title={post.title}
-                coverImage={post.coverImage}
-                date={post.date}
-                author={post.author}
-              />
-              <PostBody content={post.content} />
-            </article>
-          </>
-        )}
-      </Container>
+    <Layout>
+      <Box
+        sx={{
+          ':not(li) > ul': {
+            mb: 8
+          },
+          ':not(li) > ol': {
+            mb: 8
+          },
+          '.mdx-embed': {
+            mb: 8
+          },
+          '.slide-embed': {
+            mb: 8
+          },
+        }}
+      >
+        <Box as='article'>
+          <Heading as='h1' textAlign='center' fontSize='2xl' lineHeight='base' mb={4} ml={[7, 10]} mr={[7, 10]}>
+            {post.title}
+          </Heading>
+          <Text textAlign='center' mb={8} ml={[7, 10]} mr={[7, 10]}>
+            {post.date}
+          </Text>
+        </Box>
+        <Box bg='gray.700' p={[7, 10]} borderRadius={[0, 7]} __css={{
+          '> ul': {
+            mb: 8,
+          },
+          '> ol': {
+            mb: 8,
+            ml: 6,
+          }
+        }}>
+          <MDXRemote {...post.content} components={components} />
+        </Box>
+      </Box>
     </Layout>
   )
 }
@@ -69,7 +115,7 @@ export async function getStaticProps({ params }: Params) {
     'ogImage',
     'coverImage',
   ])
-  const content = await markdownToHtml(post.content || '')
+  const content = await serialize(post.content)
 
   return {
     props: {
