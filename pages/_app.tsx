3392526000300529cc919b7c67d20500a03f6cc2
Kentaro Matsushita <kentaro.m811@gmail.com>
2022-12-03 15:35:26 +0000

    feat: enable to render post from markdown file

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,6 +1,38 @@
 import { AppProps } from 'next/app'
+import { ChakraProvider, extendTheme } from '@chakra-ui/react'
 import '../styles/index.css'
 
+export const theme = extendTheme({
+  config: {
+    initialColorMode: 'light',
+    useSystemColorMode: true,
+  },
+  fonts: {
+    heading: "'M PLUS 2'",
+    body: "'M PLUS 2'"
+  },
+  styles: {
+    global: {
+      body: {
+        bg: 'gray.800',
+        color: 'gray.200',
+        letterSpacing: '.05em',
+      },
+    }
+  },
+  components: {
+    Link: {
+      baseStyle: {
+        color: 'purple.200',
+      },
+    },
+  }
+})
+
 export default function MyApp({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+  return (
+    <ChakraProvider theme={theme}>
+      <Component {...pageProps} />
+    </ChakraProvider>
+  )
 }
---
+++ pages/home.tsx
@@ -0,0 +1,49 @@
+import { Layout } from '../components/new/layout'
+import { Heading, Text, Box, Flex } from '@chakra-ui/react'
+import Link from 'next/link'
+import Post from '../interfaces/post'
+import { getAllPosts } from '../lib/api'
+
+type Props = {
+  allPosts: Post[]
+}
+
+export default function Home({ allPosts }: Props) {
+    return (
+      <Layout>
+        <Box>
+          {allPosts.map((post) => {
+            return (
+              <Link style={{boxShadow: `none`}} as={`/posts/${post.slug}`} href="/posts/[slug]">
+                <Flex _hover={{opacity: '0.9'}} height={175} bg='gray.700' ml={[5, 0]} mr={[5, 0]}  mb={[5, 10]} alignItems='center' justifyContent='left' borderRadius={7}>
+                <Box key={post.slug} p={8}>
+                  <Heading as='h2' fontSize='xl' lineHeight='base' mb={2}>
+                    {post.title}
+                  </Heading>
+                  <Text>
+                    {post.date}
+                  </Text>
+                </Box>
+                </Flex>
+              </Link>
+            )
+          })}
+        </Box>
+      </Layout>
+    )
+}
+
+export const getStaticProps = async () => {
+  const allPosts = getAllPosts([
+    'title',
+    'date',
+    'slug',
+    'author',
+    'coverImage',
+    'excerpt',
+  ])
+
+  return {
+    props: { allPosts },
+  }
+}
