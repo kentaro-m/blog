ceae943269831a578d3bf48156911ef3c16a9aee
Kentaro Matsushita <kentaro.m811@gmail.com>
2022-12-09 15:05:49 +0000

    style: add custom font

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,8 +1,8 @@
 import { AppProps } from 'next/app'
 import { ChakraProvider, extendTheme } from '@chakra-ui/react'
-import { BIZ_UDPGothic } from '@next/font/google'
+import { BIZ_UDGothic } from '@next/font/google'
 
-const font = BIZ_UDPGothic({ subsets: ['japanese', 'latin-ext', 'latin'], weight: ['400', '700'] })
+const font = BIZ_UDGothic({ weight: ['400', '700'] })
 
 export const theme = extendTheme({
   config: {
@@ -14,7 +14,7 @@ export const theme = extendTheme({
       body: {
         bg: 'gray.800',
         color: 'gray.200',
-        letterSpacing: '.05em',
+        letterSpacing: '.03em',
       },
     }
   },
