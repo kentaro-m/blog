daaa57f51c21750be960923cd413345b5fc1a8d3
Kentaro Matsushita <kentaro.m811@gmail.com>
2022-12-10 13:11:15 +0000

    refactor: separate post content component

--- components/PostContent.tsx
+++ components/PostContent.tsx
@@ -2,10 +2,24 @@ import React from 'react'
 import {
   Heading,
   Text,
+  Link,
+  UnorderedList,
+  OrderedList,
+  ListItem,
   Box,
+  Image,
+  Table,
+  Thead,
+  Tbody,
+  Tr,
+  Th,
+  Td,
 } from '@chakra-ui/react'
+import { MDXRemote } from 'next-mdx-remote'
+import { Slide } from './Slide'
+import { CodeBlock } from './CodeBlock'
 
-type Props = {
+type PostContentLayoutProps = {
   post: {
     title: string
     formattedDate: string
@@ -13,7 +27,7 @@ type Props = {
   children: React.ReactNode
 }
 
-export const PostContent = ({ post, children }: Props) => (
+const PostContentLayout = ({ post, children }: PostContentLayoutProps) => (
   <Box
     as='article'
     sx={{
@@ -52,3 +66,49 @@ export const PostContent = ({ post, children }: Props) => (
     </Box>
   </Box>
 )
+
+const components = (slug: string) => {
+  return {
+    a: (props: any) => <Link {...props}>{props.children}</Link>,
+    p: ({ children }: any) => <Text mb={8} lineHeight='1.75' >{children}</Text>,
+    h2: ({ children }: any) => <Heading as='h2' fontSize='2xl' lineHeight='base' mt={16} mb={8} >{children}</Heading>,
+    h3: ({ children }: any) => <Heading as='h3' fontSize='xl' lineHeight='base' mt={16} mb={8} >{children}</Heading>,
+    h4: ({ children }: any) => <Heading as='h4' fontSize='lg' fontWeight='bold' lineHeight='base' mt={16} mb={8} >{children}</Heading>,
+    ul: ({ children }: any) => <UnorderedList>{children}</UnorderedList>,
+    ol: ({ children }: any) => <OrderedList>{children}</OrderedList>,
+    li: ({ children }: any) => <ListItem mt={1} mb={1}>{children}</ListItem>,
+    code: (props: any) => <CodeBlock {...props} />,
+    inlineCode: (props: any) => <CodeBlock {...props} />,
+    img: ({ src, ...otherProps }: any) => {
+      const image = src.match(/https/) ? src : require(`../_posts/${slug}/${src}`).default
+      return (<Image
+        src={image}
+        {...otherProps}
+        mb={8}
+      />)
+    },
+    table: (props: any) => <Table {...props} mb={8} size="sm" />,
+    thead: (props: any) => <Thead {...props} />,
+    tbody: (props: any) => <Tbody {...props} />,
+    tr: (props: any) => <Tr {...props} />,
+    th: (props: any) => <Th {...props} />,
+    td: (props: any) => <Td {...props} />,
+    Slide: (props: any) => <Slide {...props} />,
+  }
+}
+
+type PostContentProps = {
+  post: {
+    title: string
+    formattedDate: string
+    slug: string
+    content: any
+  }
+}
+
+export const PostContent = ({ post }: PostContentProps) => (
+  <PostContentLayout post={{ title: post.title, formattedDate: post.formattedDate }}>
+    <MDXRemote {...post.content} components={components(post.slug)} />
+  </PostContentLayout>
+)
+
--- pages/posts/[slug].tsx
+++ pages/posts/[slug].tsx
@@ -3,59 +3,9 @@ import ErrorPage from 'next/error'
 import { getPostBySlug, getAllPosts } from '../../lib/api'
 import type PostType from '../../interfaces/post'
 import { Layout } from '../../components/Layout'
-import { CodeBlock } from '../../components/CodeBlock'
-import { Slide } from '../../components/Slide'
 import { PostContent } from '../../components/PostContent'
-import {
-  Heading,
-  Text,
-  Link,
-  UnorderedList,
-  OrderedList,
-  ListItem,
-  Box,
-  Image,
-  Table,
-  Thead,
-  Tbody,
-  Tr,
-  Th,
-  Td,
-} from '@chakra-ui/react'
 import { serialize } from 'next-mdx-remote/serialize'
-import { MDXRemote } from 'next-mdx-remote'
 import { formatDate } from '../../lib/date'
-
-const components = (slug: string) => {
-  return {
-    a: (props: any) => <Link {...props}>{props.children}</Link>,
-    p: ({ children }: any) => <Text mb={8} lineHeight='1.75' >{children}</Text>,
-    h2: ({ children }: any) => <Heading as='h2' fontSize='2xl' lineHeight='base' mt={16} mb={8} >{children}</Heading>,
-    h3: ({ children }: any) => <Heading as='h3' fontSize='xl' lineHeight='base' mt={16} mb={8} >{children}</Heading>,
-    h4: ({ children }: any) => <Heading as='h4' fontSize='lg' fontWeight='bold' lineHeight='base' mt={16} mb={8} >{children}</Heading>,
-    ul: ({ children }: any) => <UnorderedList>{children}</UnorderedList>,
-    ol: ({ children }: any) => <OrderedList>{children}</OrderedList>,
-    li: ({ children }: any) => <ListItem mt={1} mb={1}>{children}</ListItem>,
-    code: (props: any) => <CodeBlock {...props} />,
-    inlineCode: (props: any) => <CodeBlock {...props} />,
-    img: ({ src, ...otherProps }: any) => {
-      const image = src.match(/https/) ? src : require(`../../_posts/${slug}/${src}`).default
-      return (<Image
-        src={image}
-        {...otherProps}
-        mb={8}
-      />)
-    }
-    ,
-    table: (props: any) => <Table {...props} mb={8} size="sm" />,
-    thead: (props: any) => <Thead {...props} />,
-    tbody: (props: any) => <Tbody {...props} />,
-    tr: (props: any) => <Tr {...props} />,
-    th: (props: any) => <Th {...props} />,
-    td: (props: any) => <Td {...props} />,
-    Slide: (props: any) => <Slide {...props} />,
-  }
-}
 
 type Props = {
   post: PostType
@@ -70,9 +20,12 @@ export default function Post({ post }: Props) {
   }
   return (
     <Layout>
-      <PostContent post={{ formattedDate: post.formattedDate, title: post.title }}>
-        <MDXRemote {...post.content} components={components(post.slug)} />
-      </PostContent>
+      <PostContent post={{
+        formattedDate: post.formattedDate,
+        title: post.title,
+        content: post.content,
+        slug: post.slug,
+      }} />
     </Layout>
   )
 }
