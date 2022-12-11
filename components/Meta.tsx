762c61de841087d41c5d0cc7476f1c6a91bc1841
Kentaro Matsushita <kentaro.m811@gmail.com>
2022-12-11 05:00:47 +0000

    fix: add fallback if post title is empty

--- components/Meta.tsx
+++ components/Meta.tsx
@@ -14,17 +14,17 @@ export const Meta = ({ post }: Props) => {
   const router = useRouter()
   return (
     <Head>
-      <title>{post.title ? `${post.title} | ${SITE_TITLE}` : SITE_TITLE}</title>
+      <title>{post.title !== '' ? `${post.title} | ${SITE_TITLE}` : SITE_TITLE}</title>
       <meta property='og:type' content='article' />
       <meta property='og:site_name' content={SITE_TITLE} />
       <meta property='og:url' content={`${SITE_URL}${router.pathname}`} />
       <meta
         property='og:image'
-        content={`${SITE_URL}/api/og?title=${encodeURIComponent(post.title)}`}
+        content={`${SITE_URL}/api/og?title=${encodeURIComponent(post.title !== '' ? post.title : SITE_TITLE)}`}
       />
       <meta name='twitter:card' content='summary_large_image' />
       <meta name='twitter:site' content='@_kentaro_m' />
-      <meta property='og:title' content={post.title ?? SITE_TITLE} />
+      <meta property='og:title' content={post.title !== '' ? post.title : SITE_TITLE} />
     </Head>
   )
 }
