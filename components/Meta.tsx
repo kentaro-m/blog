d9e3ea7545afee1891d2b288c2eb31db5ab17774
Kentaro Matsushita <kentaro.m811@gmail.com>
2022-12-11 04:38:06 +0000

    feat: add support for ogp

--- components/Meta.tsx
+++ components/Meta.tsx
@@ -1,4 +1,5 @@
 import Head from 'next/head'
+import { useRouter } from 'next/router'
 
 type Props = {
   post: {
@@ -7,9 +8,23 @@ type Props = {
 }
 
 const SITE_TITLE = 'kentarom\'s blog'
+const SITE_URL = 'https://blog-kentarom.vercel.app'
 
-export const Meta = ({ post }: Props) => (
-  <Head>
-    <title>{post.title ? `${post.title} | ${SITE_TITLE}` : SITE_TITLE}</title>
-  </Head>
-)
+export const Meta = ({ post }: Props) => {
+  const router = useRouter()
+  return (
+    <Head>
+      <title>{post.title ? `${post.title} | ${SITE_TITLE}` : SITE_TITLE}</title>
+      <meta property='og:type' content='article' />
+      <meta property='og:site_name' content={SITE_TITLE} />
+      <meta property='og:url' content={`${SITE_URL}${router.pathname}`} />
+      <meta
+        property='og:image'
+        content={`${SITE_URL}/api/og?title=${encodeURIComponent(post.title)}`}
+      />
+      <meta name='twitter:card' content='summary_large_image' />
+      <meta name='twitter:site' content='@_kentaro_m' />
+      <meta property='og:title' content={post.title ?? SITE_TITLE} />
+    </Head>
+  )
+}
