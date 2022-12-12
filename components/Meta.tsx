bc8d1c1e854bb491d83dc0d3ce9d4b20de4feb7b
Kentaro Matsushita <kentaro.m811@gmail.com>
2022-12-12 14:27:54 +0000

    feat: add favicon settings

--- components/Meta.tsx
+++ components/Meta.tsx
@@ -25,6 +25,13 @@ export const Meta = ({ post }: Props) => {
       <meta name='twitter:card' content='summary_large_image' />
       <meta name='twitter:site' content='@_kentaro_m' />
       <meta property='og:title' content={post.title !== '' ? post.title : SITE_TITLE} />
+      <link rel="apple-touch-icon" sizes="180x180" href="/favicon/apple-touch-icon.png" />
+      <link rel="icon" type="image/png" sizes="32x32" href="/favicon/favicon-32x32.png" />
+      <link rel="icon" type="image/png" sizes="16x16" href="/favicon/favicon-16x16.png" />
+      <link rel="manifest" href="/favicon/site.webmanifest" />
+      <link rel="mask-icon" href="/favicon/safari-pinned-tab.svg" color="#5bbad5" />
+      <meta name="msapplication-TileColor" content="#da532c" />
+      <meta name="theme-color" content="#ffffff" />
     </Head>
   )
 }
