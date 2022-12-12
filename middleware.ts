14f8882cd6e3338b46aa306b102863fb5116d44a
Kentaro Matsushita <kentaro.m811@gmail.com>
2022-12-12 14:18:44 +0000

    fix: update redirect rule

--- middleware.ts
+++ middleware.ts
@@ -5,7 +5,25 @@ import type { NextRequest } from 'next/server'
  * NOTE: redirect /:path -> /posts/:path
  */
 export function middleware(request: NextRequest) {
-  return NextResponse.redirect(new URL(`/posts${request.nextUrl.pathname}`, request.url), 301)
+  const postUrlList = [
+    '/share-result-of-learning-deno',
+    '/try-the-type-challenges',
+    '/create-new-github-cli-extension',
+    '/generate-ogp-images-using-cloudinary',
+    '/we-have-held-kanazawa-js-remote-meetup-3',
+    '/we-have-held-kanazawa-js-remote-meetup-2',
+    '/joined-the-github-hackathon',
+    '/create-gatsbyjs-plugin-to-dynamically-generate-og-images',
+    '/we-have-held-kanazawa-js-meetup-1',
+    '/how-to-build-the-portfolio-for-lazy-person',
+    '/create-new-github-action'
+  ]
+
+  if (postUrlList.some(postUrl => request.nextUrl.pathname.includes(postUrl))) {
+    return NextResponse.redirect(new URL(`/posts${request.nextUrl.pathname}`, request.url), 301)
+  }
+
+  return NextResponse.next()
 }
 
 export const config = {
