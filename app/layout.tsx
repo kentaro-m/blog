988f6da60309b812cf57dc8d060f802336346df8
kentaro-m <kentaro.m811@gmail.com>
2023-07-08 20:35:00 +0900

    feat: add meta tags for layout

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,4 +1,37 @@
 import { Provider } from "./Provider"
+import { Metadata } from "next"
+
+export const metadata: Metadata = {
+  icons: [
+    {
+      rel: 'icon',
+      type: 'image/png',
+      sizes: '32x32',
+      href: '/favicon/favicon-32x32.png',
+      url: '/favicon/favicon-32x32.png',
+    },
+    {
+      rel: 'icon',
+      type: 'image/png',
+      sizes: '16x16',
+      href: '/favicon/favicon-16x16.png',
+      url: '/favicon/favicon-16x16.png',
+    },
+    {
+      rel: 'apple-touch-icon',
+      sizes: '180x180',
+      href: '/favicon/apple-touch-icon.png',
+      url: '/favicon/apple-touch-icon.png',
+    },
+    {
+      rel: 'mask-icon',
+      href: '/favicon/safari-pinned-tab.svg',
+      url: '/favicon/safari-pinned-tab.svg',
+    }
+  ],
+  manifest: '/favicon/site.webmanifest',
+  themeColor: '#ffffff',
+}
 
 export default function RootLayout({
   // Layouts must accept a children prop.
