6b1efa31d273dcf43dfd4984531d4d91807c527a
kentaro-m <kentaro.m811@gmail.com>
2023-07-16 19:50:00 +0900

    feat: add 404 not found page

---
+++ app/NotFoundPage.tsx
@@ -0,0 +1,21 @@
+'use client'
+
+import { Layout } from '../components/Layout'
+import { Box, Heading, Center } from '@chakra-ui/react'
+
+import Link from 'next/link'
+ 
+export default function NotFound() {
+  return (
+    <Layout>
+      <Box>
+        <Heading as='h1' textAlign='center' fontSize='2xl' lineHeight='base' mb={4} ml={[7, 10]} mr={[7, 10]}>
+          お探しのページは見つかりませんでした。
+        </Heading>
+        <Center>
+          <Link href="/">全ての記事をみる</Link>
+        </Center>
+      </Box>
+    </Layout>
+  )
+}
